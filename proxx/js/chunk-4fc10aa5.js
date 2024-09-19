9551cf1e8b27a51a3419709d1e4088f5980b1958
ZSCGR <932478692@qq.com>
2024-09-20 00:00:33 +0800

    fix

--- proxx/js/chunk-4fc10aa5.js
+++
@@ -1,2 +0,0 @@
-define("./chunk-4fc10aa5.js",["exports"],function(e){"use strict";e.bind=function(e,n,t){return{get:function(){var e=t.value.bind(this);return Object.defineProperty(this,n,{value:e}),e}}}});
-//# sourceMappingURL=chunk-4fc10aa5.js.map
